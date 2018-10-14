70d827a2bb6589f1e5cfe206735af31de99a404a
cheng3100 <xcchengl@126.com>
2018-10-14 18:34:12 +0800

    script backup

--- update.sh
+++ update.sh
@@ -17,8 +17,8 @@ if [ $1 = "install" ]; then
         tar -czvf vim_plugin.back.tar.gz -C ~ .vim
         rm -rf ~/.vim
     fi
-    if [ -d ~/.vimrc ];then
-        mv ~/.vimrc ~/.vimrc.back
+    if [ -f ~/.vimrc ];then
+        mv ~/.vimrc vimrc.back
         rm -f ~/.vimrc
     fi
     cp -rf .vim ~/.vim
