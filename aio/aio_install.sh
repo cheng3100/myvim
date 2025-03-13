1a642622c12fac4161e54cca5014f1b4d16d9715
cheng3100 <xcchengl@126.com>
2025-03-13 15:07:51 +0800

    aio: add aio script
    all-in-one

---
+++ aio/aio_install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# this is a all-in-one script to install all must develop tool for a new install linux machine. 
+# should include these
+
+# TODO
+#  tmux + plugin + conf
+# vim + plugin + conf
+# rustup
+# fdfind
+# fzf
+# ripgrep
+
+# DONE
+
